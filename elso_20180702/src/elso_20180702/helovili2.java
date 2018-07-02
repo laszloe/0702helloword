325ed7afd72cbac494fb607d676da82b6a03c7bc
Eva <Eva@PAALLASZLO-W10>
2018-07-02 10:53:25 +0200

    ez egy v

---
+++ elso_20180702/src/elso_20180702/helovili2.java
@@ -0,0 +1,25 @@
+package elso_20180702;
+
+public class helovili2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hellóka");
+		
+	int szam1 = 5;
+	int szam2 = 10;
+	System.out.println(szam1);
+	
+	szam2++;
+	szam2+=15;
+	System.out.println(szam2);
+	System.out.println(szam2);
+	
+	System.out.println(szam1<szam2);
+	/*
+	 * Hûdetuti és akkkor msot 
+	 */
+	
+	}
+
+}
